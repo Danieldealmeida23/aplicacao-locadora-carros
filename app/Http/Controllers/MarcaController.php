64707b3b3318c12c055316f8ddcfc2eaf44dfc17
Daniel de Almeida <122099186+Danieldealmeida23@users.noreply.github.com>
2023-12-20 00:30:18 -0300

    End-Points MARCAS
    End-Points do controlador MARCA criados finalizando a parte demonstrativa de end-Points

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -6,62 +6,106 @@ use App\Models\Marca;
 use Illuminate\Http\Request;
 
 class MarcaController extends Controller
-{
+{   
+    public function __construct(Marca $marca){
+
+        $this->marca = $marca;
+
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        //$marcas = Marca::all();
+        $marcas = $this->marca->all();
+
+        return response()->json($marcas, 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        $marca = Marca::create($request->all());
 
-        return $marca;
+
+        $request->validate($this->marca->rules(), $this->marca->feedback());
+
+        $marca = $this->marca->create($request->all());
+
+        return response()->json($marca, 201);
     }
 
     /**
      * Display the specified resource.
+     * @param Ingeter
      */
-    public function show(Marca $marca)
+    public function show($id)
     {
-        //
-    }
+        $marca = $this->marca->find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Marca $marca)
-    {
-        //
+        if($marca === null){
+            return response()->json(['erro' => 'O registro não existe !'], 404);
+        }
+        return response()->json($marca, 200);
+        
     }
 
     /**
      * Update the specified resource in storage.
+     * @param Integer
      */
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, $id)
     {
-        //
+        $marca = $this->marca->find($id);
+
+        if($marca === null)
+        {
+            return response()->json(['erro' => 'O registro não existe !'], 404);
+        }
+
+        if($request->method() === 'PATCH')
+        {
+
+            $regrasDinamicas = array();
+
+            foreach($marca->rules() as $input => $regra){
+
+                if(array_key_exists($input, $request->all())){
+
+                    $regrasDinamidas[$input] = $regra;
+
+                }
+            }
+
+            $request->validate($regrasDinamicas, $marca->feedback());
+        }else{
+            
+            $request->validate($marca->rules(), $marca->feedback());
+
+        }
+
+        $marca->update($request->all());
+
+        return response()->json($marca, 200);
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Marca $marca)
+    public function destroy($id)
     {
-        //
+        $marca= $this->marca->find($id);
+
+        if($marca === null){
+
+            return response()->json(['erro' => 'O registro não existe !'], 404);
+        }
+        $marca->delete();
+
+        return response()->json(['msg' => 'A marca foi removida com sucesso !'],200);
     }
 }
