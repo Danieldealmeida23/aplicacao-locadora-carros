7ab7f0cb091f184ef6c3252e1bd18c419a7e8c1d
Daniel de Almeida <122099186+Danieldealmeida23@users.noreply.github.com>
2023-12-21 01:12:13 -0300

    Finalização dos filtros e condições de Marcas e Modelos

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -14,11 +14,39 @@ class ModeloController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $modelo = $this->modelo->all();
+        $modelos =array();
 
-        return response()->json($modelo, 200);
+        if($request->has('atributos_marca')){
+            $atributos_marca = $request->atributos_marca;
+            $modelos = $this->modelo->with('marca:id,'.$atributos_marca);
+        }else{
+            $modelos = $this->modelo->with('marca');
+        }
+
+        if($request->has('filtro')){
+            $filtros = explode(';', $request->filtro);
+            foreach($filtros as $key => $condicao){
+                    
+                $c = explode(':', $condicao);
+                
+                $modelos = $modelos->where($c[0], $c[1], $c[2]);
+
+            }
+        }
+
+        if($request->has('atributos')){
+            $atributos = $request->atributos;
+            $modelos = $modelos->selectRaw($atributos)->get();
+
+        } else {
+            $modelos = $modelos->get();
+        }
+
+        //$modelo = $this->modelo->all();
+
+        return response()->json($modelos, 200);
     }
 
     /**
